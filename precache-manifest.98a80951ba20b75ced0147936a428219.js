47c6e4dec643fd6c16e2dd915bec945de4ce7a68
Ben Myers <ben.myers@okstate.edu>
2020-02-15 10:49:13 -0600

    Updates

---
+++ precache-manifest.98a80951ba20b75ced0147936a428219.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fd4f88686b555d11ddfee5d77025b20",
+    "url": "/d638/index.html"
+  },
+  {
+    "revision": "ec7ed0df86bb62084df4",
+    "url": "/d638/static/css/main.2739749c.chunk.css"
+  },
+  {
+    "revision": "d330cb14414ee0828bfb",
+    "url": "/d638/static/js/2.e26fff09.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/d638/static/js/2.e26fff09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec7ed0df86bb62084df4",
+    "url": "/d638/static/js/main.2b29d6f3.chunk.js"
+  },
+  {
+    "revision": "c922b0039bfbe708f6b1",
+    "url": "/d638/static/js/runtime-main.e4b2c2c0.js"
+  }
+]);
